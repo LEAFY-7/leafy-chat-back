9214dbe4ebbadd6185883ed4a54da46b83c73a5d
ohtaekwon <ohtk1119@gmail.com>
2023-07-17 21:32:58 +0900

    [ADD] token 인증 미들웨어 구현

---
+++ server/src/middlewares/token.middleware.ts
@@ -0,0 +1,33 @@
+import jsonwebtoken from "jsonwebtoken";
+import * as express from "express";
+
+const tokenDecode = (req: express.Request) => {
+  try {
+    const bearerHeader = req.headers["authorization"];
+
+    if (bearerHeader) {
+      const token = bearerHeader.split(" ")[1];
+      return jsonwebtoken.verify(token, process.env.TOKEN_SECRET!);
+    }
+    return false;
+  } catch {
+    return false;
+  }
+};
+
+const auth = async (
+  req: express.Request,
+  res: express.Request,
+  next: express.NextFunction
+) => {
+  const decodedToken = tokenDecode(req);
+  if (!decodedToken) {
+    // 인증안될때 handler
+    return;
+  }
+
+  // 모델
+  next();
+};
+
+export default { auth, tokenDecode };
