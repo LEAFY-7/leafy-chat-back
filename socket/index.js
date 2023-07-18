c80a78dd6a14c33041bf2a6dba9e03d901050abe
ohtaekwon <ohtk1119@gmail.com>
2023-07-18 20:20:20 +0900

    [ADD] 채팅소켓 연결 추가

--- socket/index.js
+++ socket/index.js
@@ -10,8 +10,30 @@ const io = new Server(PORT, {
   },
 });
 
-let onlineUsers = [];
+const chatSpace = io.of("/chat"); // 네임스페이스 등록
 
-io.on("connection", (socket) => {
+chatSpace.on("connection", (socket) => {
   console.log("connected");
+  const { watchJoin, watchSend, watchByebye } = initSocket(socket);
+  watchJoin();
+  watchSend();
+  watchByebye();
 });
+
+const initSocket = (socket) => {
+  console.log("새로운 소켓이 연결되었습니다.");
+  function watchEvent(event, func) {
+    socket.on(event, func);
+  }
+
+  function notifyToChat(event, data) {
+    console.log("chatSpace 데이터를 emit합니다.");
+    chatSpace.emit(event, data);
+  }
+
+  return {
+    watchJoin: () => {
+      watchEvent("join", async (data) => {});
+    },
+  };
+};
