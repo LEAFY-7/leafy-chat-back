596d8de68bf9c5966a50a768aa89228102193fb2
ohtaekwon <ohtk1119@gmail.com>
2023-08-06 02:47:46 +0900

    [ADD] 채팅방 찾는 미들웨어 임시 구현

--- src/routes/chat.route.ts
+++ src/routes/chat.route.ts
@@ -6,7 +6,7 @@ const router = express.Router({ mergeParams: true });
 
 router.get("/", tokenMiddleware.auth, chatController.getChattingRooms);
 router.get("/:roomId", tokenMiddleware.auth, chatController.getChattingRoom);
-router.post("/", tokenMiddleware.auth, chatController.createChattingRoom);
+router.post("/", chatController.createChattingRoom);
 router.delete(
   "/:roomId",
   tokenMiddleware.auth,
