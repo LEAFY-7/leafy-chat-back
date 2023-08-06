3f7e1c9302bc11f23033706d2663cb9dfaf9ddb4
ohtaekwon <ohtk1119@gmail.com>
2023-08-06 19:47:03 +0900

    [FIX] 경로로 인한 오류 수정

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ require("./socket");
 const { PORT } = process.env;
 const port = PORT || 5000;
 
-http.server.listen(port, () => {
+http.listen(port, () => {
   console.log(`서버 포트는 ${port} 에서 작동중입니다.`);
 });
