322a67099ebfd834c77355d2fbb05e69294f0205
ohtaekwon <ohtk1119@gmail.com>
2023-08-20 13:52:17 +0900

    [UPDATE] 소켓 모델 수정

--- src/models/socket/socket-event.model.ts
+++ src/models/socket/socket-event.model.ts
@@ -8,8 +8,12 @@ enum EventModel {
   ERROR = "error",
   JOIN_ERROR = "joinError",
   CONNECT_ERROR = "connect_error",
-  GLOBAL = "global",
-  SEND_GLOBAL = "sendChatList",
-  RECEIVE_GLOBAL = "receiveChatList",
+
+  // 유저
+  GET_USER = "get-user",
+  SEND_USER = "send-user",
+  RECEIVE_USER = "receive-user",
+  ENTER_ROOM = "enterRoom",
+  ENTER_RECEIVE_ROOM = "enterReceiveRoom",
 }
 export default EventModel;
